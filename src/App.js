ce080c62ef82aa0d39f76439b977b7d84ea4a302
haseebt456 <m.haseebtufail@gmail.com>
2023-12-27 17:01:50 +0500

    Final lab

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Mission from './views/Mission';
+import Dragon from './views/Dragon';
+import Portfolio from './views/Portfolio';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">My Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/missions">Mission</Link>
+          </li>
+          <li>
+            <Link to="/dragons">Dragons</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/">
+          <Portfolio />
+        </Route>
+        <Route path="/missions">
+          <Mission />
+        </Route>
+        <Route path="/dragons">
+          <Dragon />
+        </Route>
+      </Routes>
     </div>
+  </Router>
   );
 }
 
